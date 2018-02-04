ffa8a72ca5ec52fcc4daf69d6b4b941884d835c7
minerstat <hello@coinscrow.io>
2018-02-04 19:43:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -33,15 +33,15 @@ RESPONSE="$(wget -qO- 'https://minerstat.com/listener.php?hostname=$HOSTNAME' ; 
 
 echo "RESPONSE: $RESPONSE"
 
-if [[ "$RESPONSE" == "REBOOT" ]]; then
+if [ $RESPONSE -eq "REBOOT" ];then
 sudo reboot -f
 fi
 
-if [[ "$RESPONSE" == "REBOOT" ]]; then
+if [ $RESPONSE -eq "SHUTDOWN" ];then
 sudo shutdown -h now
 fi
 
-if [[ "$RESPONSE" == "REBOOT" ]]; then
+if [ $RESPONSE -eq "RESTART" ];then
 cd /home/minerstat/minerstat-linux
 killall node
 sleep 2
