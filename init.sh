ca4c00ea7df4c4e0b4f49f8133cad7393448f4c8
minerstat <hello@coinscrow.io>
2018-02-04 12:22:31 +0100

    some test

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ do
 
 echo "-*- BACKGROUND SERVICE -*-"
     
+#HOSTNAME
+HOSTNAME="$(hostname)"
+
 #FREE SPACE in Megabyte - SDA1
 STR1="$(df -hm | grep sda1 | awk '{print $4}')" 
 
@@ -21,6 +24,14 @@ echo "Free Space: $STR1"
 echo "CPU Usage: $STR2"
 echo "Local IP: $STR3"
 echo "Remote IP: $STR4"
+
+#SEND INFO
+wget -qO- "https://minerstat.com/getstatus.php?hostname=$HOSTNAME&space=$STR1&cpu=$STR2&localip=$STR3&remoteip=$STR4" ; echo
+
+echo "-*- MINERSTAT LISTENER -*-"
+RESPONSE="$(wget -qO- 'https://minerstat.com/listener.php?hostname=$HOSTNAME' ; echo)"
+
+echo "RESPONSE: $RESPONSE"
 
 sleep 59
 done
